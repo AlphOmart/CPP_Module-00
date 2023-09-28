5fdeb5e9036e204e466463620a422f0c08e3ebb8
AlphomArt <menelik.wubneh@gmail.com>
2023-09-28 14:08:25 +0200

    displays a message when no token is given to the program (argc == 1)

---
+++ module_1/ex00/megaphone.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+	if (argc < 2)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+}
